a8be89405cde396702daecdc421453efaeae6bab
Renan-Silva235 <renan.rubio94@gmail.com>
2026-09-09 00:01:09 -0300

    Implementado as funcionalidades da tela de login

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 // Para APIs que utilizam autenticação HTTP básica, passe a authopção. O axios irá codificar as credenciais e definir o Authorizationcabeçalho automaticamente:
 
-// const url: string = import.meta.env.VITE_API_URL;
+const url: string = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: url,
   withCredentials: true,
 });
 
