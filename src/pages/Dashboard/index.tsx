dc6950566bd4b559eedba9adab3d1cfd221d2547
Renan-Silva235 <renan.rubio94@gmail.com>
2026-09-10 14:11:01 -0300

    commit temporário

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,6 +5,7 @@ import {
   getLanguageLabel,
   languageOptions,
 } from "../../utils/languages";
+import { CardsStatistic } from "../../components/CardsStatistic";
 
 export const Dashboard = () => {
   const [selectedLanguage, setSelectedLanguage] = useState<string>("English");
@@ -14,38 +15,56 @@ export const Dashboard = () => {
   };
 
   return (
-    <div className="flex w-full">
-      <div className="flex-col">
-        <h1 className="font-bold text-color-white text-4xl font-sans">
-          Meus Decks
-        </h1>
-        <p className="text-color-silver-2 mt-2">
-          Entre em um deck para criar cards e estudar.
-        </p>
-      </div>
-      <div className="flex flex-1 justify-end">
-        <div className="relative w-48">
-          <label
-            htmlFor="languages"
-            className="flex border-2 w-full h-14 gap-2 justify-center items-center bg-linear-to-r from-btn-main-color to-second-color 
-                    text-color-white font-medium p-2 rounded-lg pointer-events-none"
-          >
-            {getLanguageLabel(selectedLanguage)} <HiChevronDown />
-          </label>
-          <select
-            id="languages"
-            value={selectedLanguage}
-            onChange={handleChange}
-            className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
-          >
-            {languageOptions.map((language) => (
-              <option key={language.id} value={language.value}>
-                {language.label}
-              </option>
-            ))}
-          </select>
+    <>
+      <div className="flex w-full">
+        <div className="flex-col">
+          <h1 className="font-bold text-color-white text-4xl font-sans">
+            Meus Decks
+          </h1>
+          <p className="text-color-silver-2 mt-2">
+            Entre em um deck para criar cards e estudar.
+          </p>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <div className="relative w-48">
+            <label
+              htmlFor="languages"
+              className="flex border-2 w-full h-14 gap-2 justify-center items-center bg-linear-to-r from-btn-main-color to-second-color 
+            text-color-white font-medium p-2 rounded-lg pointer-events-none"
+            >
+              {getLanguageLabel(selectedLanguage)} <HiChevronDown />
+            </label>
+            <select
+              id="languages"
+              value={selectedLanguage}
+              onChange={handleChange}
+              className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+            >
+              {languageOptions.map((language) => (
+                <option key={language.id} value={language.value}>
+                  {language.label}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="flex mt-11 w-full justify-center">
+        <CardsStatistic />
+      </div>
+
+      <div className="w-96">
+        <div className="mt-9">
+          <input
+            type="text"
+            placeholder="pesquisar"
+            className="w-full h-11 rounded-lg p-2.5 border border-transparent outline-none caret-color-white text-white
+                    bg-input-bg-main-color focus:bg-input-bg-main-color focus:border focus:border-color-white focus:shadow focus:shadow-color-white/35
+                      autofill:bg-input-bg-main-color"
+          />
+        </div>
+      </div>
+    </>
   );
 };
