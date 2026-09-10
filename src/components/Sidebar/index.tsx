c9d2298b71e063c1e9f4ad3e7e18d9c2b3aa47c0
Renan-Silva235 <renan.rubio94@gmail.com>
2026-09-10 11:52:55 -0300

    corrigido bugs da sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -7,40 +7,14 @@ import {
   LuChartNoAxesColumn,
 } from "react-icons/lu";
 import { MdOutlineLogout } from "react-icons/md";
-import { NavLink } from "react-router-dom";
 import { PATHS } from "../../routes/Routes";
-import type { IconType } from "react-icons/lib";
-
-interface AbasInterface {
-  icon: IconType;
-  name: string;
-  endpoint: string;
-}
-
-const Aba = ({ icon: Icon, name, endpoint }: AbasInterface) => {
-  return (
-    <NavLink to={endpoint} className="w-full">
-      {({ isActive }) => (
-        <div
-          className={`flex items-center rounded-lg justify-start w-full h-11 mt-2 pl-2 whitespace-nowrap gap-3 cursor-pointer transition-all duration-300 ${
-            isActive
-              ? "bg-btn-main-color text-color-white hover:brightness-50 scale-[0.98]"
-              : "rounded-lg bg-transparent hover:bg-white/10"
-          }`}
-        >
-          <Icon />
-          <span>{name}</span>
-        </div>
-      )}
-    </NavLink>
-  );
-};
+import { Navbar } from "../Navbar";
 
 export const Sidebar = () => {
   const { user } = useSelector((state: RootState) => state.auth);
   console.log(user?.name);
   return (
-    <aside className="flex flex-col  bg-[#070b12] h-screen w-72 p-7 pt-7 text-color-white items-center">
+    <aside className="flex flex-col  bg-[#070b12] h-screen w-72 p-7 pt-7 text-color-white items-center sticky top-0 ">
       <div className="flex flex-col items-baseline w-full">
         <h1 className="font-sans font-black tracking-tighter text-3xl">
           Flash Cards
@@ -50,16 +24,24 @@ export const Sidebar = () => {
         </p>
       </div>
       <div className="flex-1 flex-col mt-14 w-full text-center">
-        <Aba icon={LuGrid3X3} name="Dashboard" endpoint={PATHS.DASHBOARD} />
-        <Aba icon={LuCirclePlus} name="Novo Deck" endpoint={PATHS.NEW_DECK} />
-        <Aba icon={LuBookOpen} name="Estudar" endpoint={PATHS.STUDY_SESSION} />
-        <Aba
+        <Navbar icon={LuGrid3X3} name="Dashboard" endpoint={PATHS.DASHBOARD} />
+        <Navbar
+          icon={LuCirclePlus}
+          name="Novo Deck"
+          endpoint={PATHS.NEW_DECK}
+        />
+        <Navbar
+          icon={LuBookOpen}
+          name="Estudar"
+          endpoint={PATHS.STUDY_SESSION}
+        />
+        <Navbar
           icon={LuChartNoAxesColumn}
           name="Estatísticas"
           endpoint={PATHS.STATISTICS}
         />
       </div>
-      <Aba icon={MdOutlineLogout} name="Logout" endpoint={""} />
+      <Navbar icon={MdOutlineLogout} name="Logout" endpoint={""} />
     </aside>
   );
 };
