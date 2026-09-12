b39a689838b44fefaca8e8067bd2e77d5b895252
Renan-Silva235 <renan.rubio94@gmail.com>
2026-09-11 23:44:34 -0300

    implementado flags dos idiomas nos decks

--- src/components/CardsDeck/index.tsx
+++ src/components/CardsDeck/index.tsx
@@ -1,4 +1,4 @@
-import { getLanguageLabel } from "../../utils/languages";
+import { getLanguageFlag } from "../../utils/languages";
 
 interface Metadata {
   language: string;
@@ -15,7 +15,7 @@ export const CardsDeck = ({ language, title, category, counter }: Metadata) => {
                   transition-transform duration-300 hover:scale-105 hover:-translate-y-2 cursor-pointer"
     >
       <p className="font-sans tracking-wider uppercase font-bold text-second-color">
-        {getLanguageLabel(language)} {language}
+        {getLanguageFlag(language)} {language}
       </p>
       <div className="flex justify-between items-center">
         <h1 className="font-bold text-color-white text-4xl">{title}</h1>
