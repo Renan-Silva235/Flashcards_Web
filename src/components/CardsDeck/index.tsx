36d0188b2f48b69e6a782f97152063c2dc0e9b38
Renan-Silva235 <renan.rubio94@gmail.com>
2026-09-12 22:52:36 -0300

    update

--- src/components/CardsDeck/index.tsx
+++ src/components/CardsDeck/index.tsx
@@ -1,13 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import { getLanguageFlag } from "../../utils/languages";
+import { PATHS } from "../../routes/Routes";
 
 interface Metadata {
+  id?: number | null;
   language: string;
   title: string;
   category: string;
   counter: number;
 }
 
-export const CardsDeck = ({ language, title, category, counter }: Metadata) => {
+export const CardsDeck = ({
+  id,
+  language,
+  title,
+  category,
+  counter,
+}: Metadata) => {
+  const navigate = useNavigate();
+
+  const handleOpenDeck = () => {
+    navigate(
+      `${PATHS.FLASHCARDS}?deckId=${id}&deckName=${encodeURIComponent(title)}`,
+    );
+  };
   return (
     <div
       className="flex flex-col justify-between overflow-hidden h-60 border border-color-white rounded-lg w-96 p-7 
@@ -26,7 +42,10 @@ export const CardsDeck = ({ language, title, category, counter }: Metadata) => {
       </div>
       <p className="text-color-silver-2 line-clamp-2">{category}</p>
       <div className="flex justify-center gap-4 text-color-white mt-1.5">
-        <button className="bg-color-silver-1 cursor-pointer rounded-lg w-fit h-fit p-2 hover:brightness-110 scale-[0.98] transition-all duration-300">
+        <button
+          onClick={handleOpenDeck}
+          className="bg-color-silver-1 cursor-pointer rounded-lg w-fit h-fit p-2 hover:brightness-110 scale-[0.98] transition-all duration-300"
+        >
           Abrir Deck
         </button>
         <button className="bg-linear-to-r from-btn-main-color to-second-color cursor-pointer rounded-lg w-fit h-fit p-2 hover:brightness-110 scale-[0.98] transition-all duration-300">
